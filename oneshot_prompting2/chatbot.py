16ee1e30e3eba17ec39c9f988479a0cfd5fd5b61
saanvi-ch <saanvi7ch@gmail.com>
2024-09-08 00:16:23 -0700

    one shot prompting

--- oneshot_prompting2/chatbot.py
+++ oneshot_prompting2/chatbot.py
@@ -9,6 +9,7 @@ class Chatbot:
         self.client = OpenAI()
         self.model_name = model
         self.system_prompt = system_prompt
+        self.temperature = 0
         self.message_history.append({"role": "system", "content": self.system_prompt})
 
     def query(self, content):
